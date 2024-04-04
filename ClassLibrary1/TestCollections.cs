15979f580bd6ef510abbf3e2e45d8e561199f8f4
GusevaP <andrey12370@mail.ru>
2024-04-04 20:08:13 +0500

    хх

--- ClassLibrary1/TestCollections.cs
+++ ClassLibrary1/TestCollections.cs
@@ -8,7 +8,7 @@ namespace ClassLibrary1
     {
         LinkedList<Planet> col1 = new LinkedList<Planet>();
         LinkedList<string> col2 = new LinkedList<string>();
-        SortedSet<CelestialBody> col3 = new SortedSet<CelestialBody>();
+        public SortedSet<CelestialBody> col3 = new SortedSet<CelestialBody>();
         SortedSet<string> col4 = new SortedSet<string>();
         public TestCollections()
         {
@@ -16,14 +16,15 @@ namespace ClassLibrary1
             {
                 Planet planet = new Planet();
                 planet.RandomInit();
+                planet.Name = planet.Name + i.ToString();
                 if (i == 0)
                     planet = new Planet("1", 1, 1, 1, 1);
                 if (i == 500)
                     planet = new Planet("2", 2, 2, 2, 2);
                 if (i == 999)
                     planet = new Planet("3", 3, 3, 3, 3);
-                col1.AddLast(planet); 
-                col2.AddLast(planet.GetBase.ToString());
+                col1.AddLast(planet);
+                col2.AddLast(planet.ToString());
                 col3.Add(planet.GetBase);
                 col4.Add(planet.GetBase.ToString());
             }
@@ -57,6 +58,7 @@ namespace ClassLibrary1
                         ok = col3.Contains(SearchElement);
                         stopwatch.Stop();
                         time = stopwatch.ElapsedTicks;
+                        Console.WriteLine(col3.Count);
                         break;
                     }
                 case 4:  //Коллекция 4
